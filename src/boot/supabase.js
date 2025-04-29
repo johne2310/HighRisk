537d3f9bdf857ab50bf6ba614cbced71700875fd
John Evans <john@day41.com.au>
2025-04-30 09:03:31 +1000

    Update supabase.js

--- src/boot/supabase.js
+++ src/boot/supabase.js
@@ -6,8 +6,8 @@ import { boot } from 'quasar/wrappers'
 // and accessed via import.meta.env.VITE_SUPABASE_URL and import.meta.env.VITE_SUPABASE_KEY
 
 
-const supabaseUrl='https://lmvvkbraxtmilcpxbijt.supabase.co'
-const supabaseKey='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxtdnZrYnJheHRtaWxjcHhiaWp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU1NjQ2MTQsImV4cCI6MjA2MTE0MDYxNH0.xZKZFRFdmKB97sI6t7ygmwg3N1P-RUMUtsydLnopVwY'
+const supabaseUrl=
+const supabaseKey=
 
 
 
